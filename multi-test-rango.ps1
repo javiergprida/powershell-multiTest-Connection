476e4eae2fdbf0b4c8cc65504a8470f283596665
javier gomez prida <72632242+javiergprida@users.noreply.github.com>
2022-08-15 10:23:04 -0300

    Update multi-test-rango.ps1
    correcciones

--- multi-test-rango.ps1
+++ multi-test-rango.ps1
@@ -1,5 +1,5 @@
-﻿Set-ExecutionPolicy unrestricted -force
-$test = @(Get-Content -Path c:\script\servers.txt) 
+#Set-ExecutionPolicy unrestricted -force
+$test = @(Get-Content -Path c:\corona\script\servers.txt) 
 
  Write-Host "Enter the port number to check"
 [int]$portno= Read-Host
@@ -12,43 +12,38 @@ Foreach ($t in $test)
 { 
  
     $source = $t.Split('/')[0]
-
-    [int]$ipr = $t.Split('/')[1]
-
-    [int]$ipl = $source.Split('.')[3]
-
-    [int]$range = $ipr + $ipl  
     
  
   try   {
-  for ($i = $ipl ;$i -le $range; $i++){
-    $ipfinal = $source.Split('.')[0]+"."+$source.Split('.')[1]+"."+$source.Split('.')[2]
     write-Host "<----- Test-Connection Start ----->" 
 
-    write-Host "Connecting to $ipfinal.$i on port $portno"
+    write-Host "Connecting to $source on port $portno"
 
-    $test  = Test-NetConnection -ComputerName $ipfinal"."$i -Port $portno
+    $test  = Test-NetConnection -ComputerName $source -Port $portno
     if ($test:PingSucceeded -eq $true){
-        if($test:TcpTestSucceeded -eq $true){
+         
+      write-Host "PingSucceeded Is Alive $source port $portno <-Connected->`n" -ForegroundColor green
     
-        write-Host " Is Alive $ipfinal.$i port $portno <-Connected->`n" -ForegroundColor green
-        }
-     
-    }else {
-         write-Host "Not i´m dead $ipfinal.$i port $portno <-Failed->`n" -ForegroundColor red
-    } 
-     
-  } 
-  }
-  
+ 
+}else {
+     write-Host "PingSucceeded Not i´m dead $source port $portno <-Failed->`n" -ForegroundColor red
+} 
+
+  if($test:TcpTestSucceeded -eq $true){
+    
+    write-Host "TcpTestSucceeded Is Alive $source port $portno <-Connected->`n" -ForegroundColor green
+    
+ 
+}else {
+     write-Host "TcpTestSucceeded Not i´m dead $source port $portno <-Failed->`n" -ForegroundColor red
+} 
+} 
   catch [Exception] 
   { 
     Write-Host $_.Exception.GetType().FullName 
     Write-Host $_.Exception.Message 
   } 
  
-  Write-Host "<-Connected->`n" -ForegroundColor green
-} 
-
-write-Host -Prompt "<----- Test-Connection Finish ----->`n" -ForegroundColor cyan
+  write-Host -Prompt "<----- Test-Connection Finish ----->`n" -ForegroundColor cyan
 Start-Sleep -Seconds 10
+} 
